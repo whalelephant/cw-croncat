da6aa197e3db415c672157f1fc53ed8b72e0a641
Nino Lipartiia <nino.lipartiya@gmail.com>
2022-07-11 20:17:56 +0300

    small fix

--- deploy_testnet_example.sh
+++ deploy_testnet_example.sh
@@ -6,7 +6,7 @@ cargo wasm
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/rust-optimizer-arm64:0.12.6
+  cosmwasm/rust-optimizer:0.12.6
 
 NODE="--node https://rpc.uni.juno.deuslabs.fi:443"
 TXFLAG="--node https://rpc.uni.juno.deuslabs.fi:443 --chain-id uni-3 --gas-prices 0.025ujunox --gas auto --gas-adjustment 1.3 --broadcast-mode block"
@@ -17,7 +17,7 @@ AGENT="$2"
 USER="$3"
 
 # In case of M1 MacBook replace cw_croncat.wasm with cw_croncat-aarch64.wasm 
-RES=$(junod tx wasm store artifacts/cw_croncat-aarch64.wasm --from $OWNER $TXFLAG -y --output json -b block)
+RES=$(junod tx wasm store artifacts/cw_croncat.wasm --from $OWNER $TXFLAG -y --output json -b block)
 CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[0].value')
 
 # Instantiate
