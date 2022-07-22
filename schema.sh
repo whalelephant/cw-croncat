e3c67138882a37261495ee3ffdfad7b0ceea50b6
Buckram123 <m.donchenko@4irelabs.com>
2022-07-22 15:06:06 +0300

    Added workflows

---
+++ schema.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+cargo run --example schema
--- schema_and_codegen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-cargo run --example schema
-# if it fails install:
-# npm install -g cosmwasm-typescript-gen
-cosmwasm-typescript-gen generate --schema ./packages/cw-croncat-core/schema --out ./types/contract --name cw-croncat
