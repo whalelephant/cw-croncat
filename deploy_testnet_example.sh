126eb7d0a8fe02436e36a7f5d70bde015b178943
Deveus <raffree.dev@gmail.com>
2022-08-10 15:21:06 +0300

    fix: Script fix

--- deploy_testnet_example.sh
+++ deploy_testnet_example.sh
@@ -36,6 +36,7 @@ echo "Created $USER with 10 JUNOX balance"
 RES=$(junod tx wasm store artifacts/cw_croncat.wasm --from $OWNER $TXFLAG -y --output json -b block)
 CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[0].value')
 
+
 # Instantiate
 INIT='{"denom":"ujunox"}'
 junod tx wasm instantiate $CODE_ID "$INIT" --from $OWNER --label "croncat" $TXFLAG -y --no-admin
@@ -47,10 +48,17 @@ REGISTER_AGENT='{"register_agent":{}}'
 junod tx wasm execute $CONTRACT "$REGISTER_AGENT" --from $AGENT $TXFLAG -y
 
 # Create a task
-STAKE='{"create_task":{"task":{"interval":"Immediate","boundary":{},"stop_on_fail":false,"actions":[{"msg":{"staking":{"delegate":{"validator":"juno14vhcdsyf83ngsrrqc92kmw8q9xakqjm0ff2dpn","amount":{"denom":"ujunox","amount":"1000000"}}}},"gas_limit":150000}],"rules":null}}}'
+STAKE='{"create_task":{"task":{"interval":"Immediate","boundary":null,"cw20_coins":[],"stop_on_fail":false,"actions":[{"msg":{"staking":{"delegate":{"validator":"juno14vhcdsyf83ngsrrqc92kmw8q9xakqjm0ff2dpn","amount":{"denom":"ujunox","amount":"1000000"}}}},"gas_limit":150000}],"rules":null}}}'
 junod tx wasm execute $CONTRACT "$STAKE" --amount 1000000ujunox --from $USER $TXFLAG -y
 
 # proxy_call
 sleep 10      # is needed to make sure this call in the next block 
 PROXY_CALL='{"proxy_call":{}}'
 junod tx wasm execute $CONTRACT "$PROXY_CALL" --from $AGENT $TXFLAG -y
+
+echo "CONTRACT CODEID - $CODE_ID"
+echo "CONTRACT $CONTRACT"
+echo "OWNER $OWNER"
+echo "AGENT $AGENT"
+echo "USER $USER"
+
