55bc79675328db40b1297b2f102be8881a6d1538
Deveus <raffree.dev@gmail.com>
2022-08-16 20:20:52 +0300

    feat: New recurring task script

---
+++ contracts/cw-croncat/scripts/testnet_create_recurring_task.sh
@@ -0,0 +1,10 @@
+. ./testnet_init_vars.sh
+USER=juno1pd43m659naajmn2chkt6tna0uud2ywyp5dm4h3
+CONTRACT=juno12z4hh9r3j9aurjn6ppkgyjrkuu4ugrdectsh792w8feyj56dhlssvntdls
+
+RECURRING='{"create_task":{"task":{"interval":{"Block":15},"boundary":{"start":null,"end":null},"stop_on_fail":false,"actions":[{"bank":{"send":{"amount":[{"amount":"1","denom":"ujunox"}],"to_address":"juno1yhqft6d2msmzpugdjtawsgdlwvgq3samrm5wrw"}}},{"bank":{"send":{"amount":[{"amount":"1","denom":"ujunox"}],"to_address":"juno15w7hw4klzl9j2hk4vq7r3vuhz53h3mlzug9q6s"}}}],"rules":[]}}}'
+junod tx wasm execute $CONTRACT "$RECURRING" --amount 500000ujunox --from $USER $TXFLAG -y
+
+# GET_AGENT_IDS='{"get_agent_ids":{}}'
+# junod query wasm contract-state smart $CONTRACT "$GET_AGENT_IDS" $NODE
+
