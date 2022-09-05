a4058d3ce968653977019efcb8cca1e750dd8e1b
Nino Lipartiia <nino.lipartiya@gmail.com>
2022-09-05 20:28:23 +0300

    Scripts take posiitional parameters

--- contracts/cw-croncat/scripts/mainnet_instantiate.sh
+++ contracts/cw-croncat/scripts/mainnet_instantiate.sh
@@ -5,8 +5,18 @@ NODE="--node https://rpc-juno.itastakers.com:443/"
 TXFLAG="--node https://rpc-juno.itastakers.com:443/ --chain-id juno-1 --gas-prices 0.025ujuno --gas auto --gas-adjustment 1.3 --broadcast-mode block"
 
 # Make sure OWNER has some JUNO
-OWNER="$1"
-CODE_ID="$2"
+if [ -z "$1"]
+then
+    echo "Must provide owner address"
+    exit 1
+elif [ -z "$2" ]
+then
+    echo "Must provide owner code id"
+    exit 1
+else
+    OWNER="$1"
+    CODE_ID="$2"
+fi
 
 INIT='{"denom":"ujuno"}'
 junod tx wasm instantiate $CODE_ID "$INIT" --from $OWNER --label "croncat" $TXFLAG -y --no-admin
--- contracts/cw-croncat/scripts/testnet_create_staking_task.sh
+++ contracts/cw-croncat/scripts/testnet_create_staking_task.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+
+cd "$(dirname "$0")"
 . ./testnet_init_vars.sh
-USER=juno1pd43m659naajmn2chkt6tna0uud2ywyp5dm4h3
-CONTRACT=juno12z4hh9r3j9aurjn6ppkgyjrkuu4ugrdectsh792w8feyj56dhlssvntdls
+
+if [ -z "$1"]
+then
+    echo "Must provide contracts address"
+    exit 1
+elif [ -z "$2" ]
+then
+    echo "Must provide user address"
+    exit 1
+else
+    CONTRACT="$1"
+    USER="$2"
+fi
 
 STAKE='{"create_task":{"task":{"interval":"Immediate","boundary":null,"cw20_coins":[],"stop_on_fail":false,"actions":[{"msg":{"staking":{"delegate":{"validator":"juno14vhcdsyf83ngsrrqc92kmw8q9xakqjm0ff2dpn","amount":{"denom":"ujunox","amount":"1000000"}}}},"gas_limit":150000}],"rules":null}}}'
 junod tx wasm execute $CONTRACT "$STAKE" --amount 500000ujunox --from $USER $TXFLAG -y
