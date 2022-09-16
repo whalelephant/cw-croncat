937a734807bb16e969c242519dcbb2d3482732e8
Deveus <raffree.dev@gmail.com>
2022-09-16 19:43:59 +0300

    feat: More scripts

--- contracts/cw-croncat/scripts/build-amd64.sh
+++ contracts/cw-croncat/scripts/build-amd64.sh
@@ -6,4 +6,5 @@
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  --platform linux/amd64 \
   cosmwasm/workspace-optimizer:0.12.7
