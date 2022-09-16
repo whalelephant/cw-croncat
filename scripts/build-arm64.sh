2ad75ae7c6a22370f4a893f0687a073f76679b7b
Deveus <raffree.dev@gmail.com>
2022-09-16 22:19:24 +0300

    Fix: Moved scripts into root folder

--- scripts/build-arm64.sh
+++ scripts/build-arm64.sh
@@ -3,7 +3,8 @@
 # Note: the below command has amd64 as the target, meaning M1 macs
 # Please use these directions if you're not on one:
 #   https://docs.cosmwasm.com/docs/1.0/getting-started/compile-contract#optimized-compilation
-docker run --rm -v "$(pwd)":/code \
+  docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.7
+  --platform linux/arm64 \
+  cosmwasm/workspace-optimizer:0.12.6
