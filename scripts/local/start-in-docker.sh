dc6c86518b796db865a073e4a61039f687cbd550
Deveus <raffree.dev@gmail.com>
2022-09-21 00:04:54 +0300

    fix: Minor script fix

--- scripts/local/start-in-docker.sh
+++ scripts/local/start-in-docker.sh
@@ -208,16 +208,4 @@ SIMPLE_PAYROLL='{
 }'
 echo $SIMPLE_PAYROLL
 $BINARY tx wasm execute $CONTRACT_ADDRESS "$SIMPLE_PAYROLL" --amount "20000000$STAKE" --from validator $TXFLAG -y
-
-ALICE_BALANCE=$($BINARY q bank balances $($BINARY keys show alice --address))
-echo "${Green}Alice Balance :" $ALICE_BALANCE "${NoColor}"
-BOB_BALANCE=$($BINARY q bank balances $($BINARY keys show bob --address))
-echo "${Green}Bob Balance :" $BOB_BALANCE "${NoColor}"
-OWNER_BALANCE=$($BINARY q bank balances $($BINARY keys show owner --address))
-echo "${Green}Owner Balance :" $OWNER_BALANCE "${NoColor}"
-AGENT_BALANCE=$($BINARY q bank balances $($BINARY keys show agent --address))
-echo "${Green}Agent Balance :" $AGENT_BALANCE "${NoColor}"
-USER_BALANCE=$($BINARY q bank balances $($BINARY keys show user --address))
-echo "${Green}User Balance :" $USER_BALANCE "${NoColor}"
-
 echo "Done creating simple payroll"
