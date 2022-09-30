c55f8a69cd4ee724f700a80e9205fd2d36f096b4
Deveus <raffree.dev@gmail.com>
2022-09-30 21:56:44 +0300

    feat: Codegen config

---
+++ types/scripts/codegen.ts
@@ -0,0 +1,44 @@
+import codegen from "@cosmwasm/ts-codegen";
+
+codegen({
+  contracts: [
+    {
+      name: "CW-CRONCAT",
+      dir: "../packages/cw-croncat-core/schema",
+    },
+    {
+      name: "CW-RULES",
+      dir: "../packages/cw-rules-core/schema",
+    },
+  ],
+  outPath: "./contract",
+
+  // options are completely optional ;)
+  options: {
+    bundle: {
+      bundleFile: "index.ts",
+      scope: "contracts",
+    },
+    types: {
+      enabled: true,
+    },
+    client: {
+      enabled: true,
+    },
+    reactQuery: {
+      enabled: true,
+      optionalClient: true,
+      version: "v4",
+      mutations: true,
+      queryKeys: true,
+    },
+    recoil: {
+      enabled: false,
+    },
+    messageComposer: {
+      enabled: false,
+    },
+  },
+}).then(() => {
+  console.log("✨ all done!");
+});
