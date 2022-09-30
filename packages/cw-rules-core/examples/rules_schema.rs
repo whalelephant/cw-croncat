26e95b8f57297053d58ecd69274d4579cfeb2d3e
Deveus <raffree.dev@gmail.com>
2022-09-30 05:15:26 +0300

    feat: Rule contract schema(in progress)

---
+++ packages/cw-rules-core/examples/rules_schema.rs
@@ -0,0 +1,35 @@
+use cosmwasm_std::Binary;
+use std::env::current_dir;
+use std::fs::create_dir_all;
+
+use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
+use cw_rules_core::{
+    msg::{ExecuteMsg, InstantiateMsg, QueryConstruct, QueryMsg, QueryMultiResponse, RuleResponse},
+    types::{CheckOwnerOfNft, CheckProposalStatus, HasBalanceGte, Rule, Status},
+};
+
+fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("packages");
+    out_dir.push("cw-rules-core");
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(QueryMultiResponse), &out_dir);
+    export_schema(&schema_for!(QueryConstruct), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(Rule), &out_dir);
+    export_schema(&schema_for!(HasBalanceGte), &out_dir);
+    export_schema(&schema_for!(CheckOwnerOfNft), &out_dir);
+    export_schema(&schema_for!(CheckProposalStatus), &out_dir);
+
+    export_schema_with_title(
+        &schema_for!(RuleResponse<Option<Binary>>),
+        &out_dir,
+        "RuleResponse",
+    );
+    export_schema_with_title(&schema_for!(Status), &out_dir, "ProposalStatus");
+}
