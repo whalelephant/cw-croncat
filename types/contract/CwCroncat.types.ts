c43946cfafe9aca4e519173c86beddc85b58f8c4
Deveus <raffree.dev@gmail.com>
2022-10-26 23:45:20 +0300

    fix: Schema

--- types/contract/CwCroncat.types.ts
+++ types/contract/CwCroncat.types.ts
@@ -351,6 +351,7 @@ export interface Task {
   rules?: Rule[] | null;
   stop_on_fail: boolean;
   total_deposit: GenericBalance;
+  version: string;
   [k: string]: unknown;
 }
 export interface BoundaryValidated {
