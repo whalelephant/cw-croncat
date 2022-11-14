9ddd9a2e64490c47cef8b7063cb73f963892db7e
Nino Lipartiia <nino.lipartiya@gmail.com>
2022-11-14 20:40:15 +0200

    removed scripts, pushed them by accident

--- scripts/local_test/withdraw-reward.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-cd "$(dirname "$0")"
-. ./local_init_vars.sh
-
-WITHDRAW_REWARD='{"withdraw_reward":{}}'
-$BINARY tx wasm execute $CONTRACT_ADDRESS "$WITHDRAW_REWARD" --from agent $TXFLAG -y
